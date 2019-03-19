9a379c105ed15122d11f4cb3829ae3f7742e73dc
Madison Stankevich <madison.stankevich@flatironschool.com>
2019-03-19 09:01:19 -0700

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {});
 
-const listPanel = document.getElementById('list-panel')
+const showPanel = document.getElementById('show-panel')
 const list = document.getElementById('list')
 
 console.log("Program is RUNNING!!!")
@@ -21,8 +21,68 @@ function makeBookList(books){
 function addBookToList(book){
   let li = document.createElement("li");
   li.textContent = book.title;
-  list.appendChild(li);
+  li.addEventListener("click", () => {
+    displayBookInfo(book);
+  })
 
-  console.log("THIS IS BOOK:  ", book)
-  console.log("I'm in ADD_BOOK_TO_LIST")
+  list.appendChild(li);
+}
+
+function displayBookInfo(book){
+  showPanel.innerHTML = ' '
+
+  let bookTitle = document.createElement("h1");
+  let bookImg = document.createElement("img");
+  let bookDescription = document.createElement("p");
+  let bookButton = document.createElement("button");
+  let ul = document.createElement("ul");
+
+
+  let title = book["title"]
+  let image = book["img_url"]
+  let description = book["description"]
+
+  for (let user of book["users"]){
+    let li = document.createElement("li")
+    li.textContent = user.username
+    ul.append(li)
+  }
+
+  bookTitle.textContent = title
+  bookImg.setAttribute("src", image)
+  bookDescription.textContent = description
+  bookButton.textContent = "Like"
+  bookButton.addEventListener("click", () => {
+    likeBook(book)
+  })
+
+  showPanel.appendChild(bookTitle)
+  showPanel.appendChild(bookImg)
+  showPanel.appendChild(bookDescription)
+  showPanel.appendChild(ul)
+  showPanel.appendChild(bookButton)
+}
+
+function likeBook(book){
+  // let newBookUsers = book.users;
+  // console.log(newBookUsers);
+  // newBookUsers.push({"id":1, "username":"pouros"})
+  // console.log('after push', newBookUsers);
+  fetch(`http://localhost:3000/books/${book.id}`, {
+    method: "PATCH",
+    headers:
+      {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+    body:
+      JSON.stringify({
+        "users": book.users.push({"id":1, "username":"pouros"})
+        // "users": [...newBookUsers]
+    })
+  }).then(result => console.log(book.users));
+
+  displayBookInfo(book)
+  console.log("Finished the PATCH")
+
 }
